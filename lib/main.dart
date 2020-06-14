2da4efd36181e8fc5721d2905ae9b6ec6ab5dba5
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-14 20:05:11 +0530

    2.17. Adding Layout Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,54 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
-    // rn our app looking weird because as 'MaterialApp' doesnt provide default stlying and we know we that flutter controls all the pixels of our app.
-    // Scaffold : its a widget in material.dart file.
-    // it has a job for creating base page design for app. thus it gives basic design and structure and color schema gives UI for a regular app
-    // it also has couple of named arguments
-    // ctrl + space : autocompletion within a widget to see which all named argumenst we can use
+    // implementing list of string data. grouping all the data into one varaiable
+    var questions = [
+      // "\'" : "\" is escape sequnce character
+      'What\'s the use of JavaScipt?',
+      'Which programming language is used in web development?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
-        // in our app we can have appbar and then the body
-        // "{PreferredSizeWidget appBar}" : its a special kind of widget. thus either we can create our own appBar or there is inbuild 'AppBar' widget which we can use.
-        // AppBar : it again takes configration/ named agruments
-        // title: its a named argument which again take a widget
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('Hello Flutter'),
+        // there is always one body and so, within body there will be only one widget. thus, we cant have 2 text widgets with our body
+        // body: Text('Hello Flutter'), Text('Hey') //Error
+
+        // thus, we need to warp our widgets into one invisble widget
+        // column widget : if we need widgets one below the another
+        // row widget : if we need widgets beside each other
+        // both these widgets takes children as named arg.
+        // children: <Widget>[] :
+        // [] : its a list data type, its a group of data
+        // <Widget>  : its called a generic type, it a annotation to tell the type of the list.
+        // thus, it tell dart that it will hold the list of widgets
+        body: Column(
+          children: <Widget>[
+            Text('The question'),
+            // onPress : itsa function.
+            // null : it means nothing, its not a data type. (void: also means nothing but its a data type)
+            RaisedButton(
+              onPressed: null,
+              child: Text('Answer1'),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Answer2'),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Answer3'),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Answer4'),
+            ),
+            // "onPressed: null" : buttons are disabled
+          ],
+        ),
       ),
     );
   }
 }
-// note : always add comma after the closing bracket as vscode can indent the code in better way. format teh code ctrl + alt + r
