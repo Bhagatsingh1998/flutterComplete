9f6c0bd54f28a07dc29eca4c072dc1df689f181d
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-14 19:19:46 +0530

    2.15. Building a Widget Tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
-
-// OR
-
-// if we have only one expresstion in a function, dart provide us with shorthand notation with help of arrow operator.
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // adding an imp annotation '@override'
-  // although it not req, our app is working fine.
-  // '@override' : its a decorartor, provided by dart .
-    // it is just to make our code bit more cleaner and clearer.
-    // it tells that we delibrately overriding the build method which is provided by SatelessWidget. we overriding with our own implemenation and flutter forces us to override
-
   @override
   Widget build(BuildContext ctx) {
-    return MaterialApp(home: Text('Hello to flutter'));
+    // rn our app looking weird because as 'MaterialApp' doesnt provide default stlying and we know we that flutter controls all the pixels of our app.
+    // Scaffold : its a widget in material.dart file.
+    // it has a job for creating base page design for app. thus it gives basic design and structure and color schema gives UI for a regular app
+    // it also has couple of named arguments
+    // ctrl + space : autocompletion within a widget to see which all named argumenst we can use
+    return MaterialApp(
+      home: Scaffold(
+        // in our app we can have appbar and then the body
+        // "{PreferredSizeWidget appBar}" : its a special kind of widget. thus either we can create our own appBar or there is inbuild 'AppBar' widget which we can use.
+        // AppBar : it again takes configration/ named agruments
+        // title: its a named argument which again take a widget
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('Hello Flutter'),
+      ),
+    );
   }
-  // thus, if we have one function used at morethan one place, we can override and tell that we intentially overridring the previous function functianlity and not bymistake.
 }
+// note : always add comma after the closing bracket as vscode can indent the code in better way. format teh code ctrl + alt + r
