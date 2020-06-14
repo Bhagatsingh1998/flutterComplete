0d93f4254c2d6684a4b1b6829cab55d9c04e0696
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-14 22:17:18 +0530

    2.21. Updating Widget Data

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  // we want question list index automatcally get updated when button is pressed. this function in written outside of build method
+  // this is because build method gets executed whenever something is changed in the app. and hence everything is reseted and rendered again. and we dont want index of question should again reseted to 0, once the answer button is cliecked.
+  // thus, add the functionoutside of build which will be not reseted even if something is changed in app
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext ctx) {
     var questions = [
@@ -10,10 +21,9 @@ class MyApp extends StatelessWidget {
       'Which programming language is used in web development?',
     ];
 
-    // creating the function for onPressed
-    void answerQuestion() {
-      print('Answer choosen!!!');
-    }
+    // void answerQuestion() {
+    //   print('Answer choosen!!!');
+    // }
 
     return MaterialApp(
       home: Scaffold(
@@ -22,36 +32,30 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('The question'),
-            // to make button enable, we have to link onPressed with a function. thus function can be defined in 2 ways
-              // outside of class -> wrong
-              // inside the class and this is because all the compoents of widget should go in same class so that widget is stand alone unit
-            // onPressed : it takes a function with no arguments and returns nothing. (hint: hover over onPressed)
+            // accesing the question from list
+            // 1st way
+            // Text(
+            //   // "0" is the index and it has to integer. Index of list starts from zero
+            //   questions.elementAt(0),
+            // ),
+            // 2nd way
+            Text(
+              questions[0],
+            ),
             RaisedButton(
-              // linking function
-              // onPressed: answerQuestion(), //Error
-              // error : "This expression has a type of 'void' so its value can't be used." this is becasue
-              // we are executing answerQuestion() when button is pressed.
-              // wkt, dart parses the file from top to bottom, left to right. while parsing it encounters answerQuestion() and executes this function before button is pressed and it passes nothing as the value to the onPressed
-              // as the button is pressed, onPressed expects an function but as function is already executes its having the value(nothing)
-              // eg: tv and remote
-              // hece, we will pass the name of the function only
               onPressed: answerQuestion,
               child: Text('Answer1'),
             ),
             RaisedButton(
-              // above we have use named function ie, we have defined a function(head(fun name)+body)
-              // we can use anonymous function:
-              // 1 way
-              onPressed: () => print('Answer 2 pressed'),
+              // onPressed: () => print('Answer 2 pressed'),
+              onPressed: answerQuestion,
               child: Text('Answer 2'),
             ),
             RaisedButton(
-              // we can use anonymous function:
-              // 2nd way
-              onPressed: () {
-                print('Answer 3 pressed!!!');
-              },
+              // onPressed: () {
+              //   print('Answer 3 pressed!!!');
+              // },
+              onPressed: answerQuestion,
               child: Text('Answer3'),
             ),
           ],
