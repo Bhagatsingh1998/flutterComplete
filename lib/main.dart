a02069bd515ac5a3178579f8e456721536951706
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-15 10:48:19 +0530

    2.23. Updating Correctly with Stateful Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,83 @@
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   var questionIndex = 0;
+
+//   void answerQuestion() {
+//     questionIndex = questionIndex + 1;
+//     print(questionIndex);
+//   }
+
+//   @override
+//   Widget build(BuildContext ctx) {
+//     var questions = [
+//       'What\'s the use of JavaScipt?',
+//       'Which programming language is used in web development?',
+//     ];
+
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('My First App'),
+//         ),
+//         body: Column(
+//           children: <Widget>[
+//             Text(
+//               questions[0],
+//             ),
+//             RaisedButton(
+//               onPressed: answerQuestion,
+//               child: Text('Answer1'),
+//             ),
+//             RaisedButton(
+//               onPressed: answerQuestion,
+//               child: Text('Answer 2'),
+//             ),
+//             RaisedButton(
+//               onPressed: answerQuestion,
+//               child: Text('Answer3'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// changing stateless to stateful
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+// 'StatefulWidget' :it is also present inside material.dart.
+// StatefulWidget is not one class but a combination of 2 classes 
+// 1st inhertance
+class MyApp extends StatefulWidget {
+  // we will put override as 'createState' is the method which is already in 'StatefulWidget'. 
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
 
-  // we want question list index automatcally get updated when button is pressed. this function in written outside of build method
-  // this is because build method gets executed whenever something is changed in the app. and hence everything is reseted and rendered again. and we dont want index of question should again reseted to 0, once the answer button is cliecked.
-  // thus, add the functionoutside of build which will be not reseted even if something is changed in app
+// 2nd inhertance
+// State : it is also a class present from material.dart. its a generic class.
+class MyAppState extends State<MyApp> {
+
   var questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    // it takes a function which no args and it will warp the code which is changing the state.
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    // now once the index reaches 2, we will get an error as our list is having last index as 1.
     print(questionIndex);
   }
 
@@ -21,10 +88,6 @@ class MyApp extends StatelessWidget {
       'Which programming language is used in web development?',
     ];
 
-    // void answerQuestion() {
-    //   print('Answer choosen!!!');
-    // }
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -32,29 +95,19 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            // accesing the question from list
-            // 1st way
-            // Text(
-            //   // "0" is the index and it has to integer. Index of list starts from zero
-            //   questions.elementAt(0),
-            // ),
-            // 2nd way
             Text(
-              questions[0],
+              // making the index dynamic
+              questions[questionIndex],
             ),
             RaisedButton(
               onPressed: answerQuestion,
               child: Text('Answer1'),
             ),
             RaisedButton(
-              // onPressed: () => print('Answer 2 pressed'),
               onPressed: answerQuestion,
               child: Text('Answer 2'),
             ),
             RaisedButton(
-              // onPressed: () {
-              //   print('Answer 3 pressed!!!');
-              // },
               onPressed: answerQuestion,
               child: Text('Answer3'),
             ),
@@ -64,3 +117,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
