a01acaeb41b83005ed7d9c05f4a1c804e886d127
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-15 13:02:44 +0530

    2.26. Using Private Properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,32 @@
-// import 'package:flutter/material.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   var questionIndex = 0;
-
-//   void answerQuestion() {
-//     questionIndex = questionIndex + 1;
-//     print(questionIndex);
-//   }
-
-//   @override
-//   Widget build(BuildContext ctx) {
-//     var questions = [
-//       'What\'s the use of JavaScipt?',
-//       'Which programming language is used in web development?',
-//     ];
-
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('My First App'),
-//         ),
-//         body: Column(
-//           children: <Widget>[
-//             Text(
-//               questions[0],
-//             ),
-//             RaisedButton(
-//               onPressed: answerQuestion,
-//               child: Text('Answer1'),
-//             ),
-//             RaisedButton(
-//               onPressed: answerQuestion,
-//               child: Text('Answer 2'),
-//             ),
-//             RaisedButton(
-//               onPressed: answerQuestion,
-//               child: Text('Answer3'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-// changing stateless to stateful
-
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-// 'StatefulWidget' :it is also present inside material.dart.
-// StatefulWidget is not one class but a combination of 2 classes 
-// 1st inhertance
 class MyApp extends StatefulWidget {
-  // we will put override as 'createState' is the method which is already in 'StatefulWidget'. 
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MyAppState();
+    // return MyAppState();
+    // 
+    return _MyAppState();
   }
 }
 
-// 2nd inhertance
-// State : it is also a class present from material.dart. its a generic class.
-class MyAppState extends State<MyApp> {
+// if we want MyAppState class should not be used from other files ie only MyApp class can access it as both classes are in same file.
+// to indicate this, we add "_" underscore before the name(class name, variable name or function name). its a special syntax in dart which converts public class into private class 
+// class MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
+  // making variable private
+  // var questionIndex = 0;
+  var _questionIndex = 0;
+  // making changes at other places
 
-  var questionIndex = 0;
-
-  void answerQuestion() {
-    // it takes a function which no args and it will warp the code which is changing the state.
+  // making method private
+  // void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    // now once the index reaches 2, we will get an error as our list is having last index as 1.
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -96,19 +44,18 @@ class MyAppState extends State<MyApp> {
         body: Column(
           children: <Widget>[
             Text(
-              // making the index dynamic
-              questions[questionIndex],
+              questions[_questionIndex],
             ),
             RaisedButton(
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
               child: Text('Answer1'),
             ),
             RaisedButton(
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
               child: Text('Answer 2'),
             ),
             RaisedButton(
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
               child: Text('Answer3'),
             ),
           ],
